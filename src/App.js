12f790ad56df116b9b699ed45cf17cd4507c89b1
anhbd <buiducanh931@gmail.com>
2024-10-28 17:46:06 +0700

    first

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import './index.css';
+import Blog from './templates/blog/Blog.js';
+export default function App() {
+  return (
+   <div>
+    <Blog/>
+   </div>
+  );
+}
---
+++ src/templates/.eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [
+    'import', // Thêm plugin import vào cấu hình
+  ],
+  rules: {
+    'jsx-a11y/anchor-is-valid': 'off',
+    'import/prefer-default-export': 'off',
+  },
+};
