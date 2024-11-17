de50fcd2cf4594646030f1dc6ef5c93c961f5d3f
anhbd <buiducanh931@gmail.com>
2024-11-17 20:00:40 +0700

    1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
+// import * as React from 'react';
+// import * as ReactDOM from 'react-dom/client';
+// import App from './App';
+
+// const rootElement = document.getElementById('root');
+// const root = ReactDOM.createRoot(rootElement);
+
+// root.render(<App />);
 import * as React from 'react';
-import * as ReactDOM from 'react-dom/client';
+import * as ReactDOM from 'react-dom'; // Không cần import từ 'react-dom/client' trong React 17
 import App from './App';
 
 const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement);
-
-root.render(<App />);
+ReactDOM.render(<App />, rootElement); // Sử dụng ReactDOM.render thay vì createRoot
